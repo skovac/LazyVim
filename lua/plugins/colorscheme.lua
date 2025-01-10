6120e31364e18c8e882c7d33533d0535d2bebaab
Sébastien <sebastien.kovacs2@gmail.com>
2025-01-10 15:13:56 +0100

    save

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    --"ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent = false,
+      styles = {
+        sidebars = "transparent",
+        --floats = "transparent",
+      },
+    },
+  },
+}
